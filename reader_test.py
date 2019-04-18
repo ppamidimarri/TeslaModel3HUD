ae446d6c53ad1e29ae524646664d3159317162f1
pavan <pavan.pamidimarri@gmail.com>
2019-04-18 07:04:28 -0700

    Removed old code

--- reader_test.py
+++ reader_test.py
@@ -8,15 +8,13 @@ reader = canreader.CANReader()
 
 try:
 	for i in range(1, 300):
-		print("Speed: {0}, State: {5}, Gear: {1}, Hold: {2}, SOC: {3:.1f}, Capacity: {8:.1f}, Latitude: {6}, Longitude: {7}, Stamp: {4}".format(
+		print("State: {5}, Gear: {1}, Hold: {2}, Speed: {0}, SOC: {3:.1f}, Capacity: {8:.1f}, Stamp: {4}".format(
 			reader.get_speed(),
 			reader.get_gear(),
 			reader.get_brake_hold(),
 			reader.get_soc(),
 			reader.get_timestamp(),
 			reader.get_drive_state(),
-			reader.get_latitude(),
-			reader.get_longitude(),
 			reader.get_battery_capacity()
 		))
 		time.sleep(1)

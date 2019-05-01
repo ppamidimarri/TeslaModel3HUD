936cc102787c934c640df925bf9031b782227c60
pavan <pavan.pamidimarri@gmail.com>
2019-04-30 18:37:25 -0700

    Remove fluke check as values are random

--- gui_test.py
+++ gui_test.py
@@ -92,10 +92,9 @@ class HeadUpDisplay(Gtk.Window):
 			self.builder.get_object("Speed").set_markup(self.speed_markup.format(self.get_text_color(), "H"))
 			self.builder.get_object("SpeedUnit").set_markup(self.unit_markup.format(self.get_passive_text_color(), "HOLD"))
 		else:
-			if abs(self.current_speed - speed) < 20:
-				self.builder.get_object("Speed").set_markup(self.speed_markup.format(self.get_text_color(), speed))
-				self.builder.get_object("SpeedUnit").set_markup(self.unit_markup.format(self.get_passive_text_color(), "MPH"))
-				self.current_speed = speed
+			self.builder.get_object("Speed").set_markup(self.speed_markup.format(self.get_text_color(), speed))
+			self.builder.get_object("SpeedUnit").set_markup(self.unit_markup.format(self.get_passive_text_color(), "MPH"))
+			self.current_speed = speed
 		return True
 
 	def update_gear(self, gear, state):
